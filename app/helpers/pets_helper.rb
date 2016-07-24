d17cb11bf14cbd4ac0caff9d869561b0e9a01a66
Katherine Leipper <kleipper@gmail.com>
2016-07-23 21:27:36 -0700

    Display pet's age as years and months on show and index pages.

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,2 +1,5 @@
 module PetsHelper
+	def age(pet)
+		"#{ pet.age_in_years_months[0] } years, #{ pet.age_in_years_months[1] } months"
+	end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,4 +7,12 @@ class Pet < ActiveRecord::Base
 	def user_verified?(current_user)
 		current_user.id == self.user_id
 	end
+
+	def age_in_years_months
+		now = Time.now.utc.to_date
+		days_gone_by = (now - self.birthday).to_i
+		years = (days_gone_by / 365).floor # doesn't take into account leap years but also doesn't need to be super accurate for our purposes
+		months = ((days_gone_by % 365) / 30).floor # a month is typically defined as about 30 days, some might be shorter or longer in there, but it's getting rounded anyway, and doesn't need to be extremely accuate for pet bdays...
+		[years, months]
+	end
 end
