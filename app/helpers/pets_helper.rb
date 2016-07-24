33e9e062a413af206702d4a5652015c567a63262
Katherine Leipper <kleipper@gmail.com>
2016-07-24 14:04:53 -0700

    Show pet's age in years and months.

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,5 +1,9 @@
 module PetsHelper
 	def age(pet)
-		"#{ pet.age_in_years_months[0] } years, #{ pet.age_in_years_months[1] } months"
+		if pet.age_in_years_months.nil? || pet.age_in_years_months.empty?
+			"Unknown"
+		else
+			"#{ pet.age_in_years_months[0] } years, #{ pet.age_in_years_months[1] } months"
+		end
 	end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,16 +3,26 @@ class Pet < ActiveRecord::Base
 	belongs_to :gender
 	belongs_to :user
 	belongs_to :species
+	validates_presence_of :name, :gender_id, :user_id, :species_id
+	validate :birthday_must_be_past
 
 	def user_verified?(current_user)
 		current_user.id == self.user_id
 	end
 
+	def birthday_must_be_past
+		if self.birthday.present? && self.birthday > Date.today
+			errors.add(:birthday, "must be in the past")
+		end
+	end
+
 	def age_in_years_months
-		now = Time.now.utc.to_date
-		days_gone_by = (now - self.birthday).to_i
-		years = (days_gone_by / 365).floor # doesn't take into account leap years but also doesn't need to be super accurate for our purposes
-		months = ((days_gone_by % 365) / 30).floor # a month is typically defined as about 30 days, some might be shorter or longer in there, but it's getting rounded anyway, and doesn't need to be extremely accuate for pet bdays...
-		[years, months]
+		if self.birthday
+			now = Time.now.utc.to_date
+			days_gone_by = (now - self.birthday).to_i
+			years = (days_gone_by / 365).floor # doesn't take into account leap years but also doesn't need to be super accurate for our purposes
+			months = ((days_gone_by % 365) / 30).floor # a month is typically defined as about 30 days, some might be shorter or longer in there, but it's getting rounded anyway, and doesn't need to be extremely accuate for pet bdays...
+			[years, months]
+		end
 	end
 end
