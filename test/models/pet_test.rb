73c0c7b0f7d9c5485e24b8b695240875eb824260
Katherine Leipper <kleipper@gmail.com>
2016-07-24 19:47:09 -0700

    First test - check validators for creating a pet.

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test "should not save pet without a name" do
+  #   pet = Pet.new
+  #   assert_not pet.save
   # end
+
+  test "should not save pet without a name, gender, user, and species" do
+  	pet = Pet.new
+  	assert_not pet.valid?
+  	assert_equal [:name, :gender_id, :user_id, :species_id], pet.errors.keys
+  end
 end
