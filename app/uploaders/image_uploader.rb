1ea01794b0512fa02f6872a66dd3c8904ac48169
Katherine Leipper <kleipper@gmail.com>
2016-07-25 13:59:05 -0700

    Crop images to 300x300 exactly.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :medium do
-    process :resize_to_fit => [300, 300]
+    process :resize_to_fill => [300, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
