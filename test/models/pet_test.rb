4a831cf837108c174673b1e7d9138b2ba7f91d81
Katherine Leipper <kleipper@gmail.com>
2016-07-25 01:06:23 -0700

    Updated fixtures and added a couple more pet tests.

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -11,4 +11,17 @@ class PetTest < ActiveSupport::TestCase
   	assert_not pet.valid?
   	assert_equal [:name, :gender_id, :user_id, :species_id], pet.errors.keys
   end
+
+  test "should not allow pet's birthday to be in the future" do
+  	pet = pets(:future_bday_cat)
+  	assert_not pet.valid?
+  end
+
+  test "age_in_years_months should return an array of two numbers, or nil if no birthday" do
+  	pet_bday = Pet.new({:birthday => "2015-01-01"})
+  	pet_no_bday = Pet.new
+  	travel_to Time.new(2016, 7, 1, 12, 0, 0)
+  	assert_equal pet_bday.age_in_years_months, [1, 6]
+  	assert_equal pet_no_bday.age_in_years_months, nil
+  end
 end
